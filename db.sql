55bb5d1c4189499a838441ee7233df603c2ae569
dlfud <99ysl@naver.com>
2022-09-28 12:10:49 +0900

    db추가

---
+++ db.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE horang;
+USE horang;
+
+CREATE TABLE secretPost(
+    id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    content TEXT NOT NULL,
+    create_date DATETIME DEFAULT NOW(),
+    `view` INT(10) DEFAULT 1
+);
+
+SELECT * FROM secretPost;
