57741591b8593595a9ebc5a2a1c491e297e81557
Parkseonho <tjsgh2103@gmail.com>
2022-10-04 11:08:05 +0900

    INSERT 3VER

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ con.connect(function(err){
 
 });
 
+/* 쿼리문 불러오기 */ 
 app.get('/', (request, response) => {
     const sql = "select * from secretPost"
     con.query(sql, function(err, result, fields){
@@ -26,6 +27,33 @@ app.get('/', (request, response) => {
     });
 })
 
+/* 정상실행 시 console 출력 */ 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+/* 쿼리문 추가 Values.VER1*/ 
+const sqlV1 = "INSERT INTO secretPost(title, content) VALUES('오늘은 무슨날이죠?', '즐거운날..^^')"
+
+con.query(sqlV1, function(err, result, fields){
+  if (err) throw err;
+  console.log(result)
+})
+
+
+/* 쿼리문 추가 Values?.VER2*/ 
+const sqlV2 = "INSERT INTO secretPost(title, content) VALUES(?,?)"
+
+con.query(sqlV2, ['HiByeMaMa','이 드라마 재미있음'], function(err, result, fields){
+  if (err) throw err;
+  console.log(result)
+})
+
+/* 쿼리문 추가 SET?.VER*/ 
+
+const sqlSet = "INSERT INTO secretPost SET ?"
+
+con.query(sqlSet, {title:'정직한후보2',content:'재미있는듯'}, function(err, result, fields){
+  if(err) throw err;
+  console.log(result)
+});
